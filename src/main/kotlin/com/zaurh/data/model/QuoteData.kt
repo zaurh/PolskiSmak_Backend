6b41aec2b3dfe2533de2b77736a9f44d38b47af7
Zaur Hasanov <56722725+zaurh@users.noreply.github.com>
2024-07-05 11:21:51 +0200

    Update QuoteData.kt

--- src/main/kotlin/com/zaurh/data/model/QuoteData.kt
+++ src/main/kotlin/com/zaurh/data/model/QuoteData.kt
@@ -18,7 +18,35 @@ val quotes = listOf(
         author = "Anthelme Brillat—Savarin"
     ),
     QuoteData(
+        quote = "The secret ingredient is always love.",
+        author = "Wolfgang Puck"
+    ),
+    QuoteData(
         quote = "There is no sincerer love than the love of food.",
         author = "George Bernard Shaw"
     ),
+    QuoteData(
+        quote = "First we eat, then we do everything else.",
+        author = "M.F.K. Fisher"
+    ),
+    QuoteData(
+        quote = "A good cook is like a sorceress who dispenses happiness.",
+        author = "Elsa Schiaparelli"
+    ),
+    QuoteData(
+        quote = "Good food is the foundation of genuine happiness.",
+        author = "Auguste Escoffier"
+    ),
+    QuoteData(
+        quote = "A recipe has no soul. You, as the cook, must bring soul to the recipe.",
+        author = "Thomas Keller"
+    ),
+    QuoteData(
+        quote = "Food is symbolic of love when words are inadequate.",
+        author = "Alan D. Wolfelt"
+    ),
+    QuoteData(
+        quote = "Cooking is a philosophy; it's not a recipe.",
+        author = "Marco Pierre White"
+    )
 )
